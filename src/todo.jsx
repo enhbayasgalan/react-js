d5de4997357328615854b66e4ab37cfec645a08b
enhbayasgalan <enhuush0109@gmail.com>
2025-01-06 18:44:34 +0800

    Update todo.jsx

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,20 +1,49 @@
+import { useState } from "react";
 import "./todo.css";
 
 export function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if(newTodo.trim() !== "") {
+       setTodos([...todos, {text: newTodo.trim()}]);
+       setNewTodo("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const handleToggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].checked = !newTodos[index].checked;
+    setTodos(newTodos);
+  }
+   
   return (
     <div className="todo">
       <div className="container">
         <div className="box">
             <div className="todo-text">To-Do list
                 <div className="input-container">
-                    <input className="button"></input>
-                    <div className="button-2">
-                        <div className="bichig">Add</div>
-                    </div>
-                </div> 
-                <div className="filter-container">
-                <p></p>
+                    <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} className="input"/>
+                    <button onClick={handleAddTodo} className="add">Add</button>
                 </div>
+                <ul>
+                        {todos.map((todo, index) => (
+                            <li key={index} style={{display: "flex"}}>
+                              <div style={{display: "flex", alignItems: "center"}}>
+                              <input type="checkbox" checked={todo.checked} onChange={() => handleToggleTodo(index)}/>
+                                <span style={{marginRight: "50px", textDecoration: todo.checked ? "line-through" : "none"}}>{todo.text}</span>
+                                <button style={{marginTop: "5px"}} onClick={() => handleDeleteTodo(index)} className="delete">Delete</button>
+                              </div>
+                            </li>
+                        ))}
+                </ul>
             </div>
         </div>
       </div>
