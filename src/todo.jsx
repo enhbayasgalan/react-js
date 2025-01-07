df18216ce148e01580a92b6f5f9c8e403acdfe21
enhbayasgalan <enhuush0109@gmail.com>
2025-01-07 20:38:20 +0800

    Update todo.jsx

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,52 +1,90 @@
 import { useState } from "react";
 import "./todo.css";
 
+
 export function Todo() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("");
-
-  const handleAddTodo = () => {
-    if(newTodo.trim() !== "") {
-       setTodos([...todos, {text: newTodo.trim()}]);
-       setNewTodo("");
-    }
-  };
-
-  const handleDeleteTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
-  const handleToggleTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].checked = !newTodos[index].checked;
-    setTodos(newTodos);
-  }
+   const [todos, setTodos] = useState([]);
    
+   const [error, setError] = useState(false);
+
+   const [inputValue, setInputValue] = useState("");
+
+   const [filterState, setFilterState] = useState("ALL");
+   
+   const handleInputChange = (e) => {
+       setInputValue(e.target.value);
+   };
+
+   const handleAddTaskButton = () => {
+    if (inputValue.length === 0) {
+      setError(true);
+    } else {
+      setError(false);
+      setTodos([...todos, { description: inputValue,  status:"ACTIVE",}]);
+      setInputValue("");
+    }
+   };
+     
+   const handleTaskCheckBox  = (id) => {
+      const tasks = todos.map((todo) => {
+        if(todo.id === id ){
+          return{...todo, status: "COMPLETED" };
+        } else  {
+          return todo;
+        }
+      });
+      setTodos(tasks);
+   }; 
+   const handleFilterChangeState = (state) => {
+    console.log(state);
+    setFilterState(state);
+   }
+   console.log(filterState)
+
   return (
     <div className="todo">
       <div className="container">
         <div className="box">
             <div className="todo-text">To-Do list
                 <div className="input-container">
-                    <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} className="input"/>
-                    <button onClick={handleAddTodo} className="add">Add</button>
+                    {error && <div>please enter</div>}
+                    <input 
+                    placeholder={"Add task"} 
+                    value={inputValue} 
+                    className="input" 
+                    onChange={handleInputChange}
+                    />
+                    <button className="add" onClick={handleAddTaskButton}>Add</button> 
                 </div>
-                <ul>
-                        {todos.map((todo, index) => (
-                            <li key={index} style={{display: "flex"}}>
-                              <div style={{display: "flex", alignItems: "center"}}>
-                              <input type="checkbox" checked={todo.checked} onChange={() => handleToggleTodo(index)}/>
-                                <span style={{marginRight: "50px", textDecoration: todo.checked ? "line-through" : "none"}}>{todo.text}</span>
-                                <button style={{marginTop: "5px"}} onClick={() => handleDeleteTodo(index)} className="delete">Delete</button>
-                              </div>
-                            </li>
-                        ))}
-                </ul>
+                <div className="all">
+                      <div onClick={() => handleFilterChangeState("ALL")} style={{color: filterState === "ALL" ? "blue" : "black"}}>ALL</div>
+                      <div onClick={() => handleFilterChangeState("ACTIVE")} style={{color: filterState === "ACTIVE" ? "blue" : "black"}}>ACTIVE</div>
+                      <div onClick={() => handleFilterChangeState("COMPLETED")} style={{color: filterState === "COMPLETED" ? "blue" : "black"}}>COMPLETED</div>
+                </div>
+                <div className="filter-container">
+                {todos.filter((todo) => {
+                  if(filterState === "ACTIVE"){
+                    return todo.status === "ACTIVE"; 
+                  }else if (filterState === "COMPLETED") {
+                    return todo.status === "COMPLETED"; 
+                  } else {
+                    return true;
+                  }
+                })
+                .map((todo) => {
+                      return (
+                        <div className="enter">
+                        <input type="checkbox" onChange={() => handleTaskCheckBox(todo.id)}/>
+                        {todo.description}
+                        </div>
+                      );  
+                    })}
+                </div>
+                
             </div>
         </div>
       </div>
     </div>
   );
 }
+
