868a2be0f66715e4ebf52af78dbbc49cc26a2167
enhbayasgalan <enhuush0109@gmail.com>
2025-01-08 18:27:00 +0800

    Update todo.jsx

--- src/todo.jsx
+++ src/todo.jsx
@@ -10,7 +10,9 @@ export function Todo() {
    const [inputValue, setInputValue] = useState("");
 
    const [filterState, setFilterState] = useState("ALL");
+
    
+
    const handleInputChange = (e) => {
        setInputValue(e.target.value);
    };
@@ -24,6 +26,13 @@ export function Todo() {
       setInputValue("");
     }
    };
+
+   const handleDeleteTodo = (todo) => {
+    const newTodos = [...todos];
+    newTodos.splice(todo, 1);
+    setTodos(newTodos);
+   }
+   
      
    const handleTaskCheckBox  = (id) => {
       const tasks = todos.map((todo) => {
@@ -57,9 +66,10 @@ export function Todo() {
                     <button className="add" onClick={handleAddTaskButton}>Add</button> 
                 </div>
                 <div className="all">
-                      <div onClick={() => handleFilterChangeState("ALL")} style={{color: filterState === "ALL" ? "blue" : "black"}}>ALL</div>
-                      <div onClick={() => handleFilterChangeState("ACTIVE")} style={{color: filterState === "ACTIVE" ? "blue" : "black"}}>ACTIVE</div>
-                      <div onClick={() => handleFilterChangeState("COMPLETED")} style={{color: filterState === "COMPLETED" ? "blue" : "black"}}>COMPLETED</div>
+                      <button onClick={() => handleFilterChangeState("ALL")} style={{color: filterState === "ALL" ? "blue" : "black"}} className="AL">ALL</button>
+                      <button onClick={() => handleFilterChangeState("ACTIVE")} style={{color: filterState === "ACTIVE" ? "blue" : "black"}} className="AL">ACTIVE</button>
+                      <button onClick={() => handleFilterChangeState("COMPLETED")} style={{color: filterState === "COMPLETED" ? "blue" : "black"}} className="AL">COMPLETED</button>
+                      <button onClick={() => handleFilterChangeState("LOG")} style={{color: filterState === "LOG" ? "blue" : "black"}} className="AL">LOG</button>
                 </div>
                 <div className="filter-container">
                 {todos.filter((todo) => {
@@ -74,17 +84,22 @@ export function Todo() {
                 .map((todo) => {
                       return (
                         <div className="enter">
-                        <input type="checkbox" onChange={() => handleTaskCheckBox(todo.id)}/>
-                        {todo.description}
+                          <div style={{display: "flex", gap: "20px"}}>
+                              
+                            <input type="checkbox" onChange={() => handleTaskCheckBox(todo.id)}/>
+                            <p style={{maxWidth: "200px", overflow: "scroll"}}>   {todo.description}</p>
+                     
+                          </div>
+                       
+                        <button onClick={() => handleDeleteTodo(todo)} className="delete" >Delete</button>
                         </div>
                       );  
                     })}
-                </div>
-                
+                 </div>
+                 <p className="p">No tasks yet. Add one above!</p>
             </div>
         </div>
       </div>
     </div>
   );
 }
-
